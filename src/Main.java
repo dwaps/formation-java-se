f24d57fbc3bdb0654590848bc9c2849f1cd57db1
Michael Cornillon <contact@dwaps.fr>
2017-08-24 09:21:52 +0200

    Calculatrice simple : choix de l'opération à 2 entiers

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,8 @@ public class Main {
 		
 		boolean nouveauCalcul = true;
 		Scanner sc = new Scanner(System.in);
-		String reponse = "";
-		int  nb1 = 0, nb2 = 0;
+		String reponse = "", operation = "", error = "";
+		int  nb1 = 0, nb2 = 0, resultat = 0;
 		
 		System.out.println(TITRE_APPLICATION[0] + "\n" + TITRE_APPLICATION[1] + "\n" + TITRE_APPLICATION[2]);
 		
@@ -21,19 +21,44 @@ public class Main {
 			System.out.print("Premier nombre : ");
 			nb1 = sc.nextInt();
 			
+			System.out.print("Type d'opération [+/-*%] : ");
+			operation = sc.next();
+			
 			System.out.print("Deuxième nombre : ");
 			nb2 = sc.nextInt();
 			sc.nextLine();
 			
-			System.out.println("Résultat de l'opération : " + (nb1+nb2));
+			switch(operation) {
+				case "+":
+					resultat = nb1 + nb2;
+					break;
+				case "/":
+					resultat = nb1 / nb2;
+					break;
+				case "-":
+					resultat = nb1 - nb2;
+					break;
+				case "*":
+					resultat = nb1 * nb2;
+					break;
+				case "%":
+					resultat = nb1 % nb2;
+					break;
+				default:
+					error = "Désolé, je ne peux pas faire le calcul...";
+			}
+			
+			System.out.println((!error.equals("")) ? error : "Résultat de l'opération : " + resultat);
 			
 			
-			System.out.println("Veux-tu effectuer une nouvelle opération ? (oui ou non)");
+			System.out.println("Veux-tu effectuer une nouvelle opération ? [On]");
 			reponse = sc.nextLine();
 			
-			if(reponse.equals("non")) {
+			if(reponse.equals("n")) {
 				nouveauCalcul = false;
 			}
+			
+			error = "";
 		}
 		
 		System.out.println(FIN_APPLICATION);
