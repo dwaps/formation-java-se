021c0a96095dfe736df419c59ff7b7220f179615
Michael Cornillon <contact@dwaps.fr>
2017-08-24 10:04:47 +0200

    Calculatrice simple : calcul sur une seule ligne à 2 entiers

--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,88 @@ public class Main {
 
 	public static void main(String[] args) {
 		final String TITRE_APPLICATION[] = {
-				"------------",
-				"CALCULATRICE",
-				"------------"
+			"------------",
+			"CALCULATRICE",
+			"------------"
+		};
+		final String ERRORS[] = {
+			"Ton opération n'est pas logique !",
+			"Désolé, je ne peux pas faire le calcul...",
+			"Navré mais je ne peux que faire des opérations à 2 entiers...",
+			"Utiliser les espaces sinon je ne peux pas faire le calcul !"
 		};
 		final String FIN_APPLICATION = "La calculatrice a bien été fermée ! ;)";
 		
-		boolean nouveauCalcul = true;
+		boolean nouveauCalcul = true, stopOperation = false;
+		int nb1 = 0, nb2 = 0,resultat = 0, index = 0;
 		Scanner sc = new Scanner(System.in);
-		String reponse = "", operation = "", error = "";
-		int  nb1 = 0, nb2 = 0, resultat = 0;
+		
+		String reponse = "";
+		String operation = "";
+		String error = "";
 		
 		System.out.println(TITRE_APPLICATION[0] + "\n" + TITRE_APPLICATION[1] + "\n" + TITRE_APPLICATION[2]);
 		
-		while(nouveauCalcul) {
-			System.out.print("Premier nombre : ");
-			nb1 = sc.nextInt();
+		while (nouveauCalcul) {
+			System.out.print("Ton opération : ");
+			operation = sc.nextLine();
 			
-			System.out.print("Type d'opération [+/-*%] : ");
-			operation = sc.next();
+			String[] tab = operation.split(" ");
 			
-			System.out.print("Deuxième nombre : ");
-			nb2 = sc.nextInt();
-			sc.nextLine();
-			
-			switch(operation) {
-				case "+":
-					resultat = nb1 + nb2;
-					break;
-				case "/":
-					resultat = nb1 / nb2;
-					break;
-				case "-":
-					resultat = nb1 - nb2;
-					break;
-				case "*":
-					resultat = nb1 * nb2;
-					break;
-				case "%":
-					resultat = nb1 % nb2;
-					break;
-				default:
-					error = "Désolé, je ne peux pas faire le calcul...";
+			if (tab.length == 3) {
+				for (String item : tab) {
+					
+					try {
+						if (nb1 == 0) {
+							nb1 = Integer.parseInt(item);
+						} else {
+							nb2 = Integer.parseInt(item);
+							stopOperation = true;
+						}
+					} catch(NumberFormatException e) {
+						if (index % 2 == 1) {
+							operation = item;
+						} else {
+							error = ERRORS[0];
+						}
+					}
+					
+					if (nb1 != 0 && nb2 != 0) {
+						switch (operation) {
+							case "+":
+								resultat = nb1 + nb2;
+								break;
+							case "-":
+								resultat = nb1 - nb2;
+								break;
+							case "/":
+								resultat = nb1 / nb2;
+								break;
+							case "*":
+								resultat = nb1 * nb2;
+								break;
+							case "%":
+								resultat = nb1 % nb2;
+								break;
+							default:
+								error = ERRORS[1];
+						}
+					}
+					
+					if (stopOperation) {
+						break;
+					}
+					
+					index++;
+				}
+				
+			} else if (tab.length > 3) {
+				error = ERRORS[2];
+			} else {
+				error = ERRORS[3];
 			}
 			
 			System.out.println((!error.equals("")) ? error : "Résultat de l'opération : " + resultat);
-			
 			
 			System.out.println("Veux-tu effectuer une nouvelle opération ? [On]");
 			reponse = sc.nextLine();
@@ -58,10 +94,17 @@ public class Main {
 				nouveauCalcul = false;
 			}
 			
+			stopOperation = false;
+			operation = "";
+			resultat = 0;
+			nb1 = 0;
+			nb2 = 0;
+			index = 0;
 			error = "";
 		}
 		
 		System.out.println(FIN_APPLICATION);
+		sc.close();
 	}
 
 }
