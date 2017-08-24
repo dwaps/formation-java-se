6a93a7d310ea775af26968c7a0e11f0821e8b24c
Michael Cornillon <contact@dwaps.fr>
2017-08-24 13:51:28 +0200

    Calculatrice simple : optimisation du code --> classes

---
+++ src/Constants.java
@@ -0,0 +1,17 @@
+
+public final class Constants {
+	
+	public final static String[] TITRE_APPLICATION = {
+		"------------",
+		"CALCULATRICE",
+		"------------"
+	};
+	
+	public final static String[] ERRORS = {
+		"Ton opération n'est pas logique !",
+		"Désolé, je ne peux pas faire le calcul...",
+		"Navré mais je ne peux que faire des opérations à 2 entiers...",
+		"Utilise les espaces sinon je ne peux pas faire le calcul !"
+	};
+	
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		final String TITRE_APPLICATION[] = getTitle();
+		final String TITRE_APPLICATION[] = Constants.TITRE_APPLICATION;
 		final String FIN_APPLICATION = "La calculatrice a bien été fermée ! ;)";
 		
 		boolean nouveauCalcul = true;
@@ -11,7 +11,8 @@ public class Main {
 		
 		String reponse = "";
 		String operation = "";
-		String error = "";
+		
+		Calcul calcul = new Calcul();
 		
 		System.out.println(TITRE_APPLICATION[0] + "\n" + TITRE_APPLICATION[1] + "\n" + TITRE_APPLICATION[2]);
 		
@@ -20,7 +21,7 @@ public class Main {
 			operation = sc.nextLine();
 			
 			String[] tab = operation.split(" ");
-			calculate(tab, error, operation);
+			calcul.calculate(tab, operation);
 			
 			System.out.println("Veux-tu effectuer une nouvelle opération ? [On]");
 			reponse = sc.nextLine();
@@ -30,99 +31,10 @@ public class Main {
 			}
 			
 			operation = "";
-			error = "";
 		}
 		
 		System.out.println(FIN_APPLICATION);
 		sc.close();
-	}
-	
-	static String[] getTitle() {
-		String[] title = {
-			"------------",
-			"CALCULATRICE",
-			"------------"
-		};
-		
-		return title;
-	}
-	
-	static String[] getErrors() {
-		String[] errors = {
-			"Ton opération n'est pas logique !",
-			"Désolé, je ne peux pas faire le calcul...",
-			"Navré mais je ne peux que faire des opérations à 2 entiers...",
-			"Utiliser les espaces sinon je ne peux pas faire le calcul !"
-		};
-		
-		return errors;
-	}
-	
-	static int calculate(String[] tab, String error, String operation) {
-		String ERRORS[] = getErrors();
-		boolean stopOperation = false;
-		
-		int nb1 = 0,
-			nb2 = 0,
-			resultat = 0,
-			index = 0;
-
-		if (tab.length == 3) {
-			for (String item : tab) {
-				
-				try {
-					if (nb1 == 0) {
-						nb1 = Integer.parseInt(item);
-					} else {
-						nb2 = Integer.parseInt(item);
-						stopOperation = true;
-					}
-				} catch(NumberFormatException e) {
-					if (index % 2 == 1) {
-						operation = item;
-					} else {
-						error = ERRORS[0];
-					}
-				}
-				
-				if (nb1 != 0 && nb2 != 0) {
-					switch (operation) {
-						case "+":
-							resultat = nb1 + nb2;
-							break;
-						case "-":
-							resultat = nb1 - nb2;
-							break;
-						case "/":
-							resultat = nb1 / nb2;
-							break;
-						case "*":
-							resultat = nb1 * nb2;
-							break;
-						case "%":
-							resultat = nb1 % nb2;
-							break;
-						default:
-							error = ERRORS[1];
-					}
-				}
-				
-				if (stopOperation) {
-					break;
-				}
-				
-				index++;
-			}
-			
-		} else if (tab.length > 3) {
-			error = ERRORS[2];
-		} else {
-			error = ERRORS[3];
-		}
-		
-		System.out.println((!error.equals("")) ? error : "Résultat de l'opération : " + resultat);
-		
-		return resultat;
 	}
 
 }
