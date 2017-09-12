09cfdb2d4b23a19113696f8496e9555d2adc8266
Michael Cornillon <contact@dwaps.fr>
2017-09-12 11:00:35 +0200

    Dessin d'une maison

---
+++ src/fr/dwaps/javase/Main.java
@@ -0,0 +1,12 @@
+package fr.dwaps.javase;
+import fr.dwaps.classes.Draw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Draw draw = new Draw();
+		
+		draw.house(true);
+	}
+
+}
